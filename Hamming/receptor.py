c36402d67232396db1b1f6910a7c71437d89c395
eliasalvarado <77988653+eliasalvarado@users.noreply.github.com>
2024-08-01 17:47:21 -0600

    Update receptor.py

--- Hamming/receptor.py
+++ Hamming/receptor.py
@@ -1,5 +1,8 @@
-def hammingReciever(message):
-    def correct_block(block):
+import socket
+
+
+def verificarIntegridad(message):
+    def correjirMensaje(block):
         n = len(block)
         parityNum = [2**i for i in range(n) if 2**i <= n]
         
@@ -13,6 +16,7 @@ def hammingReciever(message):
                 error += par
 
         if error != 0:
+            print("Error en el bit ", error, " del bloque ", block)
             block[error - 1] = str(int(block[error - 1]) ^ 1)
 
         originalMessage = []
@@ -26,10 +30,46 @@ def hammingReciever(message):
     corrected_message = []
 
     for block in blocks:
-        corrected_message.append(correct_block(list(block)))
+        corrected_message.append(correjirMensaje(list(block)))
 
     return "".join(corrected_message)
 
+def recibirInformacion(ip, port):
+    print("Esperando conexión...")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((ip, port))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Conexión entrante desde ', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            message = data.decode()
+            return message[:-1]
+
+def decodificarMensaje(messageBinario):
+    message = ""
+    for i in range(0, len(messageBinario), 8):
+        message += chr(int(messageBinario[i:i+8], 2))
+    return message
+    
+
 if __name__ == "__main__":
-    message = input("Ingrese el mensaje recibido: ")
-    print("Mensaje decodificado: ", hammingReciever(message))
+    ip = "127.0.0.1"
+    port = 65432
+
+    # Capa transmisión
+    messageBinario = recibirInformacion(ip, port)
+    print("Mensaje recibido en binario: ", messageBinario)
+
+    # Capa de enlace
+    messageHamming = verificarIntegridad(messageBinario)
+    print("Mensaje recibido con Hamming: ", messageHamming)
+
+    # Capa de presentación
+    message = decodificarMensaje(messageHamming)
+
+    # Capa de aplicación
+    print("Mensaje recibido: '", message, "'")
