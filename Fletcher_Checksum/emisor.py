0cbd922535122e60afc309008dd5c9da5c5e81b8
eliasalvarado <77988653+eliasalvarado@users.noreply.github.com>
2024-08-01 17:08:25 -0600

    Update emisor.py

--- Fletcher_Checksum/emisor.py
+++ Fletcher_Checksum/emisor.py
@@ -1,3 +1,5 @@
+import socket
+import random
 
 def fletcher16(message):
     sum1 = 0
@@ -7,15 +9,51 @@ def fletcher16(message):
         sum2 = (sum2 + sum1) % 255
     return (sum2 << 8) | sum1
 
-def emisor(data):
+def calcularIntegridad(data):
     message = [int(data[i:i+8], 2) for i in range(0, len(data), 8)]
 
     checksum = fletcher16(message)
 
     return "".join(data) + format(checksum, '016b')
 
-if __name__ == '__main__':
-    data = input("Introduce el mensaje: ")
-    print(emisor(data))
+def stringToBinary(data):
+    return ''.join(format(ord(i), '08b') for i in data)
 
+def ruido(message, probability):
+    for i in range(len(message)):
+        if random.random() < probability:
+            message[i] ^= str(int(message[i]) ^ 1)
+    return message
+
+def enviarInformacion(ip, port, message):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((ip, port))
+        s.sendall(message.encode())
+    print("Mensaje exitosamente enviado.")
+
+if __name__ == '__main__':
+    # Capa de aplicación
+    data = input("Introduce el mensaje: ")
+    
+    # Capa de presentación
+    data = stringToBinary(data + "\n\n")
+    print("Mensaje en binario: ", data)
+
+    # Capa de enlace
+    message = calcularIntegridad(data)
+    print("Mensaje con Fletcher-16: ", message)
+    ip = "127.0.0.1"
+    port = 65432
+
+    # Ruido
+    probability = float(input("Introduce la probabilidad de ruido en su mensaje (0-1): "))
+    if probability < 0 or probability > 1:
+        print("Probabilidad inválida")
+        exit()
+
+    message = ruido(message, probability)
+    print("Mensaje con ruido: ", message)
+
+    # Capa transmisión
+    enviarInformacion(ip, port, message)
 
